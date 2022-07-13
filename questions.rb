53a61656553650fa3b7ac5e394b64709ba349a54
AA Student <appacademy@Mini-31.local>
2022-07-13 12:07:58 -0700

    separate tables in separate files

--- questions.rb
+++ questions.rb
@@ -29,6 +29,14 @@ class User
 end
 
 class Question
+    attr_accessor :id, :title, :body, :author_id 
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end 
 
     def self.find_by_id
         data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
@@ -37,6 +45,13 @@ class Question
 end
 
 class QuestionFollow
+    attr_accessor :id, :question_follows, follower_id
+
+    def initialize(options)
+        @id = options['id']
+        @question_follows = options['question_follows']
+        @follower_id = options['follower_id']
+    end 
 
     def self.find_by_id
         data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
@@ -45,6 +60,17 @@ class QuestionFollow
 end
 
 class Reply
+    def initialize(options)
+        attr_accessor :id, :parent_id, :question_id, :writer_id, :title, :body
+
+        @id = options['id']
+        @parent_id = options['parent_id']
+        @question_id = options['question_id']
+        @writer_id = options['writer_id']
+        @title = options['title']
+        @body = options['body']
+
+    end 
 
     def self.find_by_id
         data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
@@ -53,6 +79,13 @@ class Reply
 end
 
 class QuestionLike
+    def initialize(options)
+        attr_accessor :id, :writer_id, :question_id 
+
+        @id = options['id']
+        @writer_id = options['writer_id']
+        @question_id = options['question_id']
+    end 
 
     def self.find_by_id
         data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes")
